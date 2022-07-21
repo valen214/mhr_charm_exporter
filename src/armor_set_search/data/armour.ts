6d30bc58c697f38138d6b53ede6997a36949cc6a
valen214 <xvalen214x@gmail.com>
2022-07-21 09:27:08 +0800

    add rust, wasm for armor set skill search

---
+++ src/armor_set_search/data/armour.ts
@@ -0,0 +1,14 @@
+
+
+export type Armor = [
+  Name: string,
+  ...Data: number[],
+  Slots: [ number, number, number ],
+  Skills: object, // skills
+  Defense: number, // defense
+  Resistances: [ number, number, number, number, number ]
+];
+
+export const DEFAULT_ARMOR: readonly Armor[]  = Object.freeze([
+  ["AAAA",3,3,1,15,99,0,8,0,1,[3,0,0],{"BBBB":1,"CCCC":1,"DDDD":2},130,[2,1,3,-5,2]]
+])
