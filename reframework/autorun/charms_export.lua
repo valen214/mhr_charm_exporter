dd1f1735b24ce54c9f2bc2c6e6e7990dd57e259c
valen214 <xvalen214x@gmail.com>
2022-08-11 06:21:06 +0800

    add armor export

--- reframework/autorun/charms_export.lua
+++ reframework/autorun/charms_export.lua
@@ -2,8 +2,22 @@
 
 log.info("[Charms Export] initializing...")
 
-
+local ARMOR_ID_TYPE = 2
 local TALISMAN_ID_TYPE = 3
+
+-- https://stackoverflow.com/a/27028488/3142238
+function json_dump(o)
+  if type(o) == 'table' then
+     local s = '{ '
+     for k,v in pairs(o) do
+        if type(k) ~= 'number' then k = '"'..k..'"' end
+        s = s ..k..':' .. json_dump(v) .. ', '
+     end
+     return string.sub(s, 1, -3) .. ' } '
+  else
+     return tostring(o)
+  end
+end
 
 
 local _log = function(str)
@@ -42,15 +56,81 @@ local charmToString = function(item)
 
   repr = repr .. table.concat(decoSlots, ",", 1, 3)
 
-
-
-
   return repr
 end
 
-local function getCharmsStringList()
-  local charmsStringList = {}
 
+--[[
+
+  armorData: snow.data.ArmorData
+
+    armorData:call("get_AllDecoSlotList"):call("get_Item", 0) returns
+    snow.data.equip.param.DecorationSlotsData
+
+    armorData:call("get_AllSkillDataList")
+      returns snow.data.PlSkillData
+
+
+      
+    local skillData = allSkillDataList:call("get_Item", i || 0);
+      _log(
+        tostring(skillData:call("get_EquipSkillId")) .. " " ..
+        tostring(skillData:call("get_Name")) .. " " ..
+        tostring(skillData:call("get_TotalLv")) .. " " ..
+        tostring(skillData:call("get_EffectiveLv")) .. " " ..
+        tostring(skillData:call("get_MaxLv"))
+      );
+
+--]]
+local armorToString = function(item)
+  -- local isArmor = item:call("isArmor")
+  -- _log(tostring(isArmor));
+  -- _log(tostring(item:call("getName")))
+  -- _log(tostring(item:call("getArmorData")))
+
+  local armorData = item:call("getArmorData"); -- snow.data.ArmorData
+  local allDecoSlotList = armorData:call("get_AllDecoSlotList");
+  local allSkillDataList = armorData:call("get_AllSkillDataList");
+
+  local skillObject = {}
+  local skillIdObject = {}
+
+  local count = allSkillDataList:call("get_Count")
+  for i = 0,count-1,1 do
+    local skillData = allSkillDataList:call("get_Item", i);
+    if skillData:call("get_EquipSkillId") ~= 0 then
+      skillObject[skillData:call("get_Name")] = skillData:call("get_TotalLv")
+      skillIdObject[skillData:call("get_EquipSkillId")] = skillData:call("get_TotalLv")
+    end
+  end
+
+  local armorString = string.format(
+    "\"%s\", [%d, %d, %d], %s, %s",
+    armorData:call("getName"),
+    allDecoSlotList:call("get_Item", 2):call("getSlotLv"),
+    allDecoSlotList:call("get_Item", 1):call("getSlotLv"),
+    allDecoSlotList:call("get_Item", 0):call("getSlotLv"),
+    json_dump(skillObject),
+    json_dump(skillIdObject)
+  )
+
+  _log(armorString);
+
+
+  return armorString
+
+  -- tostring(item:call("get_BuildupPoint"))
+end
+
+
+
+local function getCharmsStringList()
+  local output = {}
+  local charmsStringList = {}
+  local armorsStringList = {}
+
+  output["charms"] = charmsStringList
+  output["armors"] = armorsStringList
 
   local DataManager = sdk.get_managed_singleton("snow.data.DataManager");
   if not DataManager then
@@ -62,14 +142,25 @@ local function getCharmsStringList()
   
   local InventoryList = EquipmentBox:get_field("_WeaponArmorInventoryList");
   
-  
+  local first = true;
   
   local itemCount = InventoryList:call("get_Count");
   setmetatable(charmsStringList, { __shl = function (t,v) t[#t+1]=v end })
+  setmetatable(armorsStringList, { __shl = function (t,v) t[#t+1]=v end })
   
   for i = 0,itemCount-1,1 do
     local item = InventoryList:call("get_Item(System.Int32)", i);
     local itemType = item:get_field("_IdType");
+
+    if itemType == 2 then
+      _= armorsStringList << armorToString(item)
+      
+
+      if first then
+        first = false;
+        _log(armorToString(item));
+      end
+    end;
   
     
     if itemType == TALISMAN_ID_TYPE then -- Talisman: 3
@@ -78,7 +169,7 @@ local function getCharmsStringList()
   end
 
   -- _log(table.concat(charmsStringList, "\n"))
-  return charmsStringList
+  return output
 end
 
 
@@ -89,22 +180,33 @@ local saveToFile = package.loadlib(
 
 
 
-local outputFileName = "exported_charms.txt"
+local charmsFileName = "exported_charms.txt"
+local armorsFileName = "exported_armors.txt"
 re.on_draw_ui(function()
   if imgui.button(
-    "[Charms Export] export charms to reframework/data/" .. outputFileName
+    "[Charms Export] export charms & armor to " ..
+    "reframework/data/" .. charmsFileName .. " and " ..
+    "reframework/data/" .. armorsFileName
   ) then
     
     -- local output = table.concat(charmsStringList, "\n");
     local output = getCharmsStringList() or {}
 
+    local charmsStringList = output["charms"] or {}
+    local armorsStringList = output["armors"] or {}
+
     if saveToFile then
       saveToFile(
-          "reframework/data/" .. outputFileName,
-          table.concat(output, "\n")
+          "reframework/data/" .. charmsFileName,
+          table.concat(charmsStringList, "\n")
+      );
+      saveToFile(
+          "reframework/data/" .. armorsFileName,
+          table.concat(armorsStringList, "\n")
       );
     else
-      json.dump_file(outputFileName, output)
+      json.dump_file(charmsFileName, charmsStringList)
+      json.dump_file(armorsFileName, armorsStringList)
     end
 
     --[[
@@ -113,5 +215,6 @@ re.on_draw_ui(function()
   end
 end)
 
+_log("HEEYEY22331551");
 
 log.info("[Charms Export] initialized.")
